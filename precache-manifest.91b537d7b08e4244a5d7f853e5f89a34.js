c77203c2343df242ba31c869524d40114b51a1d8
Svet <stsupryk@gmail.com>
2019-11-26 23:28:17 +0200

    Updates

---
+++ precache-manifest.91b537d7b08e4244a5d7f853e5f89a34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19f9d7d3ba6d6d1b15caca114a1ba4f",
+    "url": "/React_Uber/index.html"
+  },
+  {
+    "revision": "d3b8ce580f218778b5cb",
+    "url": "/React_Uber/static/css/main.fe5b35f0.chunk.css"
+  },
+  {
+    "revision": "32500309647fe1f3f300",
+    "url": "/React_Uber/static/js/2.ab572d63.chunk.js"
+  },
+  {
+    "revision": "d3b8ce580f218778b5cb",
+    "url": "/React_Uber/static/js/main.29659767.chunk.js"
+  },
+  {
+    "revision": "985b6cbd81ae0e715934",
+    "url": "/React_Uber/static/js/runtime~main.bccd551d.js"
+  }
+]);
